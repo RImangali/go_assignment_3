c701029a9bb5a8992a360a0e7df8ed6a55b2befd
RImangali <158676473+RImangali@users.noreply.github.com>
2024-04-27 23:13:13 +0500

    fixed

--- dtos/book.go
+++
@@ -1,9 +0,0 @@
-package dtos
-
-type Book struct {
-	Title         string  `json:"title" db:"title"`
-	AuthorName    string  `json:"author_name" db:"author_name"`
-	AuthorSurname string  `json:"author_surname" db:"author_surname"`
-	Date          string  `json:"date" db:"date"`
-	Rating        float32 `json:"rating" db:"rating"`
-}
