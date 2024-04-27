3f9d7ba2eeb8effa645ea36ba09ebd9320b6df02
RImangali <158676473+RImangali@users.noreply.github.com>
2024-04-27 18:41:32 +0500

    fixed

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,24 @@
-package cmd
+package main
 
 import (
 	"log"
-    "github.com/gin-gonic/gin"
-    "github.com/go-redis/redis"
+	"github.com/go_assignment_3/server"
+	"github.com/go_assignment_3/app"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-    db, err := DBConnectionBuilder()
-	redis := RedisConnectionBuilders()
+	db, err := app.DBConnectionBuilder()
+	redis := app.RedisConnectionBuilders()
 	
 	if err != nil {
 		log.Panicf("Some error with connetion to DB: %s", err.Error())
 	}
 
-	handler := HandlerBuilder(db, redis)
+	handler := app.HandlerBuilder(db, redis)
 	router := handler.RouterBuilder()
 
-	err = SeverConnectionBuilder(router)
+	err = server.SeverConnectionBuilder(router)
 	if err != nil {
 		log.Panicf("Some error with Run to DB: %s", err.Error())
 	}
